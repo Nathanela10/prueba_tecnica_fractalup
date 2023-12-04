ebe7adb9f2c8ca1993b2ce20780e9982d6bbe462
jonathanvra <jonathan.vra@gmail.com>
2023-12-04 13:50:26 -0500

    feat: Add redirect and 404 page

---
+++ src/pages/NotFound/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="not-found">
+    <h1>Error 404</h1>
+    <p>Página no encontrada</p>
+    <router-link to="/">Volver al Home</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+};
+</script>
+
+<styles scoped src="./NotFound.css"></styles>
--- src/router.js
+++ src/router.js
@@ -3,14 +3,17 @@ import Router from 'vue-router';
 import Home from './pages/HomeProject/HomeProject.vue';
 import ViewOne from './pages/ViewOne.vue';
 import ViewTwo from './pages/ViewTwo.vue';
+import NotFound from './pages/NotFound/NotFound.vue';
 
 Vue.use(Router);
 
 export default new Router({
   mode: 'history',
   routes: [
+    { path: '/', redirect: '/home' },
     { path: '/home', component: Home },
     { path: '/viewOne', component: ViewOne },
     { path: '/viewTwo', component: ViewTwo },
+    { path: '*', component: NotFound}
   ],
 });
